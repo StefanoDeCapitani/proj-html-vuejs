13e04b4aae370bb7b5038d5f5bbe2a574812d25e
Stefano De Capitani <stefano.deca@live.com>
2021-11-22 20:58:02 +0100

    Fontawesome installation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@fontsource/dm-sans";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPhoneAlt, faArrowRight);
 
 Vue.config.productionTip = false;
 
