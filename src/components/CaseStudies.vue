5e19c6161fef1927afb49f4205e6fe5fb6d657d8
Stefano De Capitani <stefano.deca@live.com>
2021-11-23 14:51:59 +0100

    Case Studies component created

--- src/components/CaseStudies.vue
+++ src/components/CaseStudies.vue
@@ -1,28 +1,37 @@
 <template>
   <div class="container">
-    <h2 class="text-xxl">Trusted by Leading Organizations</h2>
+    <h2 class="text-xxl text-center">Trusted by Leading Organizations</h2>
     <div class="row-cols-3">
       <div class="col">
-        <img src="../assets/img/case-studies-1.jpg" alt="Case Study 1" />
-        <p class="text-xl">
+        <ArcticleCard
+          imgSrc="../assets/img/case-studies-1.jpg"
+          imgAlt="Case Study 1"
+          arcticleHref="#"
+        >
           How Spaces attracted five million visitors by improving the content
-        </p>
+        </ArcticleCard>
         <p class="text-xxl">200%</p>
         <p>Higher revenue from digital</p>
       </div>
       <div class="col">
-        <img src="../assets/img/case-studies-4.jpg" alt="Case Study 2" />
-        <p class="text-xl">
+        <ArcticleCard
+          imgSrc="../assets/img/case-studies-4.jpg"
+          imgAlt="Case Study 2"
+          arcticleHref="#"
+        >
           Creativity helped Hemisferio to increase their brand reach vertically
-        </p>
+        </ArcticleCard>
         <p class="text-xxl">10x</p>
         <p>Sales increase with the same ad spend</p>
       </div>
       <div class="col">
-        <img src="../assets/img/case-studies-6.jpg" alt="Case Study 3" />
-        <p class="text-xl">
+        <ArcticleCard
+          imgSrc="../assets/img/case-studies-6.jpg"
+          imgAlt="Case Study 3"
+          arcticleHref="#"
+        >
           How DigitalBox used AI-powered data insight to boost sales
-        </p>
+        </ArcticleCard>
         <p class="text-xxl">3-year</p>
         <p>Partnership with Avada Consultant</p>
       </div>
@@ -32,9 +41,15 @@
 </template>
 
 <script>
+import ArcticleCard from "./ArcticleCard.vue";
 export default {
   name: "CaseStudies",
+  components: {
+    ArcticleCard,
+  },
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+@import "../assets/styles/utilities";
+</style>
